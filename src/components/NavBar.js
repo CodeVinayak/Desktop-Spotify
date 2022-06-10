4a2d37b3741b186799e796a7298e3749b46aa12a
Vinayak Singh <vinayaksingh10142@gmail.com>
2022-06-10 19:50:57 +0530

    updated 0.01

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,7 +9,7 @@ const NavBar = () => {
             <div className="app-header">{metadata.appName}</div>
             <div className="nav-links">
                 <a
-                    href="https://www.github.com/jessej-samuel/spotipy"
+                    href="https://github.com/CodeVinayak/spotipy"
                     target="_blank"
                     rel="noreferrer"
                 >
